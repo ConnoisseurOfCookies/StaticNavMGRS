033d5d00ea7a6ae374558e01b7694a0c9edcce76
ConnoisseurOfCookies <67559796+ConnoisseurOfCookies@users.noreply.github.com>
2022-05-19 01:12:19 +1000

    Made boilerplate, started writing tests

---
+++ staticNavModel.js
@@ -0,0 +1,86 @@
+
+
+
+function getStaticNavGrid(gridOrigin, distance, bearing){
+    
+    let gridVector = gridToVector(gridOrigin);
+
+    let radian = milToRadian(bearing);
+
+    let unitVector = getUnitVector(radian);
+
+    let mainVector = [unitVector[0]*distance, unitVector[1]*distance];
+
+    let resultGridVector = [mainVector[0]+gridVector[0], mainVector[1] + gridVector[1]];
+
+    return vectorToGrid(resultGridVector);
+}
+
+function gridIsValid(grid){
+    return true;
+}
+
+function gridToVector(grid){
+
+}
+
+function milToRadian(bearing){
+
+}
+
+function getUnitVector(angle){
+
+}
+
+function vectorToGrid(){
+    
+}
+
+
+
+const unitTests = {
+    Tests: {
+        testGridToVector: () => {
+            if(typeof gridToVector === 'undefined'){
+                console.log("gridToVector is not defined");
+                return;
+            }
+
+            let grid1 = "332666";
+            console.assert(gridToVector(grid1) === [33200, 66600], `gridToVector does not convert ${grid1} to vector`);
+        },
+        testVectorToGrid: () => {
+            if(typeof vectorToGrid === 'undefined'){
+                console.log('vectorToGrid has not been defined');
+                return;
+            }
+        },
+        testMilToRadian: () => {
+            if(typeof milToRadian === 'undefined'){
+                console.log('milToRadian has not been defined');
+                return;
+            }
+        },
+        testGetUnitVector: () => {
+            if(typeof getUnitVector === "undefined"){
+                console.log('getUnitVector has not been defined');
+                return;
+            }
+        },
+        testGridIsValid: () => {
+            if(typeof gridIsValid === 'undefined'){
+                console.log('gridIsValid has not been defined');
+                return;
+            }
+        }
+    },
+    Run: () => {
+        unitTests.Tests.testGridToVector();
+        unitTests.Tests.testVectorToGrid();
+        unitTests.Tests.testMilToRadian();
+        unitTests.Tests.testGetUnitVector();
+        unitTests.Tests.testGridIsValid();
+    }
+}
+
+unitTests.Run();
